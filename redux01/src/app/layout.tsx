e17cad890138b328d1f260444589ef8bb506ee5a
Nandana <nandana@vaniyathans.com>
2024-10-23 10:42:38 +0530

    Add contents of redux01 folder

---
+++ redux01/src/app/layout.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { Provider } from 'react-redux';
+import store from '../store/store';
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <Provider store={store}>
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    </Provider>
+  );
+}
+
+
+
---
+++ redux01/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Counter from './components/Counter';
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Welcome to the Redux Toolkit with Next.js</h1>
+      <Counter />
+    </main>
+  );
+}
+
+
